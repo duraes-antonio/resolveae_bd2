5253a2676cc8fc39671b774e2b45eafd6560d95a
Elimar Macena <elimar.macena@gmail.com>
2018-10-21 20:51:55 -0300

    verificacao do commit anterior

--- VIEWS/view_dados_contrato.sql
+++ VIEWS/view_dados_contrato.sql
@@ -1,4 +1,4 @@
---INFORMACOES ESTABELECIDAS NO CONTRATO
+--INFORMACOES ESTABELECIDAS NO CONTRATO.
 CREATE VIEW VIEW_DADOS_CONTRATO AS (
 SELECT CT.id,US2.nome AS CONTRATANTE, US1.nome AS PRESTADOR, SE.titulo , CT.descricao AS CONTRATO, CT.horas_contratadas, SE.valor, CT.data_inicio,CT.data_fim,CT.data_ult_modif FROM servico AS SE
   INNER JOIN contrato AS CT ON CT.id = SE.fk_contrato
--- VIEWS/view_endereco_usuario.sql
+++ VIEWS/view_endereco_usuario.sql
@@ -1,4 +1,4 @@
---ENDERECO COM BASE NO USUARIO
+--ENDERECO COM BASE NO USUARIO.
 CREATE VIEW VIEW_ENDERECO_USUARIO AS(
 SELECT U.id,U.nome, concat(B.nome,', ',CD.nome,', ',ES.nome) AS endereco  FROM usuario AS U
 INNER JOIN endereco AS E ON E.fk_usuario = U.id
