bb9f8cf4f42b8c85b051884e9b8d30ad3b4ce0ef
Elimar Macena <elimar.macena@gmail.com>
2018-10-27 23:20:38 -0300

    ADD: funcoes media de valor de um prestador e servicos em aberto de um prestador

---
+++ FUNCTIONS/GET_MEDIA_VALOR_HORA.sql
@@ -0,0 +1,5 @@
+﻿CREATE FUNCTION GET_MEDIA_VALOR_HORA (id_usuario INT, OUT valorMedio FLOAT) AS
+'
+SELECT (SE.valor/COUNT(*))AS ValorMedioHora FROM servico AS SE
+WHERE SE.fk_idUsuario = id_usuario
+'LANGUAGE SQL;
---
+++ FUNCTIONS/GET_SERVICOS_ABERTOS.sql
@@ -0,0 +1,7 @@
+﻿CREATE FUNCTION GET_SERVICOS_ABERTOS(IN id_usuario INT )
+  RETURNS TABLE ( idContrato INT) AS
+  '
+  SELECT CO.id FROM contrato AS CO
+  INNER JOIN servico AS SO ON SO.fk_contrato = CO.id
+  WHERE SO.fk_usuario = id_usuario AND CO.data_fim IS NULL
+  ' LANGUAGE SQL;
